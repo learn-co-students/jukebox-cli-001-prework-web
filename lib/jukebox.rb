71a325f851d3a6b9ef01b00babd0d3d7e8dfd7ee
jeffslutzky <jeffslutzky@gmail.com>
2015-09-04 22:43:20 -0400

    done

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,49 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def run(songs)
+  puts "Please enter a command:"
+  command = gets.downcase.strip
+  case command
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "exit"
+      exit_jukebox
+    else
+      run(songs)   
+  end
+end
+
+def help
+  puts "I accept the following commands:"
+  puts "help"
+  puts "list"
+  puts "play"
+  puts "exit"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  selection = gets.downcase.strip
+  songs.each_with_index do |song, index|
+    if selection == song.downcase || selection.to_i == index + 1
+      puts song
+      # return
+    else puts "Invalid input, please try again"
+    end 
+  end
+end
+
+def exit_jukebox
+    puts "Goodbye."
+    return
+end
