4c8aff707a1d47c36a06eef51dd88750e2576933
seanliu93 <sean93liu@gmail.com>
2015-12-08 17:33:03 -0500

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,75 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  #this method should be the same as in jukebox.rb
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+
+
+def list(my_songs)
+  #this method is different! Collect the keys of the my_songs hash and 
+  #list the songs by name
+  count = 1
+  my_songs.each {|key, value|
+    puts count.to_s << ". " << key
+    count = count + 1
+  }
+end
+
+
+def play(my_songs)
+  #this method is slightly different!
+  #you should still ask the user for input and collect their song choice
+  #this time, only allow user's to input a song name
+  #check to see if the name they give is in fact a key of the my_songs hash
+  #if it isn't, tell them their choice is invalid
+  #if it is, play the song using the system 'open <file path>' syntax
+  #get the file path of the song by looking it up in the my_songs hash
+  puts "Please enter a song name or number:"
+  songname = gets.chomp
+  valid = 0
+  my_songs.each_with_index {|val, index|
+    if songname == val || songname.to_i == index+1
+      puts "Playing " << val
+      valid = 1
+    end
+  }
+  if valid == 0
+    puts "Invalid input, please try again"
+  end
+
+end
+
+def exit_jukebox
+  #this method is the same as in jukebox.rb
+  puts "Goodbye"
+end
+
+def run(my_songs)
+  #this method is the same as in jukebox.rb
+  help
+  resp = ""
+  loop  do
+    puts "Please enter a command:"
+    resp = gets.chomp
+    case resp
+      when "list"
+        list(songs)
+      when "play"
+        play(songs)
+      when "help"
+        help
+      when "exit"
+        exit_jukebox
+        break
+      else
+        puts "Not a valid command."
+    end
+  end
+end
