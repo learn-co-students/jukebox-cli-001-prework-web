6cf2a6f1985fb9581e8e43a88f5fa55fb66441cb
Adam King <adam.king0126@gmail.com>
2015-12-12 03:08:28 +0000

    finish basic jukebox

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -21,17 +23,45 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   choice = gets.chomp
-  songs.each do |number, title|
-    (choice.to_i - 1 == number || choice == v) ? (puts "Playing #{songs[title]}") : (puts "Invalid input, please try again")
+  valid = false
+  songs.each_with_index do |song, index|
+    if (choice.to_i - 1 == index)
+      puts "Playing #{songs[index]}"
+      valid = true
+    elsif (choice == song)
+      puts "Playing #{choice}"
+      valid = true
+    end
   end
+  puts "Invalid input, please try again" if valid == false
 end
 
 def list(songs)
   songs.each_with_index{|song, index| puts "#{index + 1}. #{song}"}
 end
 
-def exit(arg)
+def exit_jukebox
   puts "Goodbye"
 end
+
+def run(songs)
+  help
+  loop do
+    puts "Please enter a command:"
+    input = gets.chomp
+    case input
+    when 'play'
+      play(songs)
+    when 'help'
+      help
+    when 'list'
+      list(songs)
+    when 'exit'
+      break
+    end
+  end
+  exit_jukebox
+end
+
 
 
