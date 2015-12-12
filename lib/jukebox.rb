784d65d2fa2e766ccfe7dcc906f066407af90388
Adam King <adam.king0126@gmail.com>
2015-12-12 01:13:30 +0000

    working through it

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,28 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  choice = gets.chomp
+  songs.each do |number, title|
+    (choice.to_i - 1 == number || choice == v) ? (puts "Playing #{songs[title]}") : (puts "Invalid input, please try again")
+  end
+end
+
+def list(songs)
+  songs.each_with_index{|song, index| puts "#{index + 1}. #{song}"}
+end
+
+def exit(arg)
+  puts "Goodbye"
+end
+
+
