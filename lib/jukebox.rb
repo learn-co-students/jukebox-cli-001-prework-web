15382b76f7b7656fa007b31556898f486a5cca01
Nathaniel Miller <dukeoflaser@gmail.com>
2015-12-23 01:27:51 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,12 +1,58 @@
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
+require 'pry'
+require 'pry-nav'
 
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  
+  puts "Please enter a song name or number:"
+  re = gets.chomp
+  song_num = (1..songs.size)
+  
+  re.to_i > 0 ? index = re.to_i - 1 : index = songs.find_index(re)
+  chosen_song = songs[index] unless index == nil 
+  
+  if song_num === re.to_i || songs.include?(re)
+    puts "Playing #{chosen_song}"
+  else
+    puts "Invalid input, please try again"
+  end
+  
+end
+
+def list(songs)
+  songs.each_with_index { |song, i| puts "#{i + 1}. #{song}" }
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  loop do
+    puts "Please enter a command:"
+    command = gets.chomp
+    unless command == "exit"
+      case command
+      when "list"
+        list
+      when "play"
+        play
+      when "help"
+        help
+      else
+        puts "Invalid input, please try again"
+      end
+    else
+      exit_jukebox
+      break
+    end
+  end
+end
