467b1a2ba871d3815b2a998c4c10820d65eb0504
SaiedAbbasi <saiedabbasijr@gmail.com>
2016-01-30 14:07:19 -0500

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,58 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+  numbered = []
+  songs.each.with_index(1) do |title, index|
+  numbered << "#{index}. #{title}"
+  end
+  puts numbered
+end
+
+def play(songs)
+  a=*(1..10)
+  phrase = ""
+  puts "Please enter a song name or number:"
+  users_request = gets.chomp
+              #songs.inlude?("users_request") || [1..10].inlude?("users_request")  ? puts "Playing <song name>" : puts "Invalid input, please try again"
+    if songs.include?("#{users_request}")
+      puts "Playing #{users_request}"
+    elsif a.include?(users_request.to_i)
+      phrase << songs[(users_request.to_i-1)]  
+      puts "Playing #{phrase}"
+    else
+      puts "Invalid input, please try again"  
+    end  
+  
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  users_help_response = ""
+  until users_help_response == "exit" do
+    puts "Please enter a command:"
+    users_help_response = gets.chomp
+    if users_help_response == "list"
+      list(songs)
+    elsif users_help_response == "play"
+      play(songs)
+    else #users_help_response == "help"    
+      help
+    end  
+  end
+  exit_jukebox  
+
+
+end  
