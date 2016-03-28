95e320d4e06fbb4257485f29e1294964ba41bcd8
PoloPro <wesleybdavis@gmail.com>
2016-03-28 01:11:45 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -2,28 +2,24 @@
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+my_songs = {
+            "Go Go GO" => '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/jukebox-cli/audio/Emerald-Park/01.mp3',
+            "LiberTeens" => '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/jukebox-cli/audio/Emerald-Park/02.mp3',
+            "Hamburg" =>  '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/jukebox-cli/audio/Emerald-Park/03.mp3',
+            "Guiding Light" => '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/jukebox-cli/audio/Emerald-Park/04.mp3',
+            "Wolf" => '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/jukebox-cli/audio/Emerald-Park/05.mp3',
+            "Blue" => '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/jukebox-cli/audio/Emerald-Park/06.mp3',
+            "Graduation Failed" => '/home/nitrous/code/labs/jukebox-cli-001-prework-web/audio/Emerald-Park/jukebox-cli/audio/Emerald-Park/07.mp3'
+}
 
 def help
   #this method should be the same as in jukebox.rb
-
 end
-
-
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
 end
-
 
 def play(my_songs)
   #this method is slightly different!
@@ -33,7 +29,6 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
 end
 
 def exit_jukebox
