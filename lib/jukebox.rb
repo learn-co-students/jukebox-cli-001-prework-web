80a8d0aa6416132e6212774901d1d9be41e656a0
kallaugher <alicekallaugher@gmail.com>
2016-04-07 21:18:36 -0400

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,12 +1,56 @@
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
 
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  ARGV.clear
+  user_response = gets.chomp
+
+  if songs.include?(user_response) == true 
+    puts "Playing #{user_response}"
+  elsif user_response.to_i.between?(1, 10) == true
+    puts "Playing #{songs[user_response.to_i - 1]}"
+  elsif songs.include?(user_response) == false
+    puts "Invalid input, please try again"
+  end
+
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  user_response = gets.chomp
+
+  while user_response != "exit"
+
+  case user_response
+  when "list"
+    list(songs)
+  when "play"
+    play(songs)
+  when "help"
+    help
+  end
+
+  puts "Please enter a command:"
+    user_response = gets.chomp
+  end
+
+  exit_jukebox
+
+end
