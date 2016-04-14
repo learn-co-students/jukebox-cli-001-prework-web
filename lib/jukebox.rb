6f22a4a07f680c84fb36d651721238160d0a1b33
abillig <avrambillig@gmail.com>
2016-04-14 19:10:11 -0400

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,6 +22,46 @@ def help
 "
 end
 
-def run(input)
-help
+def play(songs)
+  puts "Please enter a song name or number:"
+  songentry=gets.chomp
+
+  songs.each_with_index do |song, index|
+    if song == songentry
+      puts "Playing #{song}"
+    elsif 
+      index == songentry.to_i
+      puts "Playing #{songs[index-1]}"
 end
+end
+        puts "Invalid input, please try again"
+ end
+
+
+
+def list(songs)
+songs.each_with_index do |song, index|
+puts "#{index+1}. #{song}"
+end
+end
+
+def exit_jukebox
+puts "Goodbye"
+end      
+
+
+def run(songs)
+help
+puts "Please enter a command:"
+command = gets.chomp
+  if command == "list"
+    list(songs)
+  elsif command == "play"
+    play(songs)
+  elsif command == "help"
+    help
+  elsif command == "exit"
+    exit_jukebox
+end
+end
+
