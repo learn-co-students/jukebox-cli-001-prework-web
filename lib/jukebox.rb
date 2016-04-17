6ac6773915cd4db7a1b621c677170be22f19e827
colbyschulz <colbyschulz@gmail.com>
2016-04-16 20:43:03 -0500

    jukebox cli

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -9,4 +10,64 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+
+def help
+  puts "I accept the following commands:"
+  puts " - help : displays this help message"
+  puts " - list : displays a list of songs you can play"
+  puts " - play : lets you choose a song to play"
+  puts " - exit : exits this program"
+end
+
+def list(songs)
+  songs.each.with_index(1) do |song, index| 
+    puts "#{index}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_choice = gets.chomp
+  if (1..9).include?(user_choice.to_i)
+    puts "Playing #{songs[user_choice.to_i-1]}"
+  elsif songs.include?(user_choice)
+    puts "Playing #{user_choice}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+  
+
+def exit_jukebox 
+  puts "Goodbye"
+end
+
+def run(songs) 
+  input = nil
+  help
+  while input != "exit"
+  puts "Please enter a command:"
+  input = gets.chomp
+    if input == "help"
+      help
+    elsif input == "list"
+      list(songs)
+    elsif input == "play"
+      play(songs)
+    elsif input == "exit"
+      exit_jukebox
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
 
