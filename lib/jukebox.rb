96a19e3d337c6a7909f09042f69d2ece6be6cb37
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-07-24 14:41:35 -0400

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,92 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+=begin def say_hello(name)
+  "Hi #{name}!"
+end
+ 
+puts "Enter your name:"
+users_name = gets.chomp
+ 
+puts say_hello(users_name)
+
+=end
+def run(songs)
+  y = 0
+
+  while y < 1
+  help
+  puts "Please enter a command:"
+  user_input = gets.chomp
+  puts "You selected: #{user_input}"
+
+    if user_input == "help"
+      help
+    elsif user_input == "list"
+      list(songs)
+    elsif user_input == "play"
+      play(songs)
+    elsif user_input == "exit"
+      exit_jukebox
+      y = 1
+    end
+  end
+
+end
+
+def help 
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+
+def list(songs)
+  x = 0
+  while x < songs.length
+    puts "#{x + 1}. #{songs[x]}"
+    x = x + 1
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  response = gets.chomp
+  invalid = true
+  number = true
+
+  songs.each do |song|
+    if song == response
+      invalid = false
+      number = false
+    end
+  end
+
+if number 
+  z = 0
+  while z < songs.length
+    puts z
+    number = response.to_i
+    puts "number = #{number}"
+    number = number - 1
+    if z == number
+      response = songs[z]
+      invalid = false
+    end
+    z = z + 1
+  end
+end
+
+  if invalid
+    puts "Invalid input, please try again"
+  else 
+    puts "Playing #{response}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
