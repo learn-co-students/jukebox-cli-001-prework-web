cca4ab1a722ca70d5ae1533c991e8afcac7bac2c
mo6709 <mo6709@gmail.com>
2016-07-27 18:32:53 -0400

    Done Include Advance Task

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,12 +1,62 @@
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
+def help
+   puts "I accept the following commands:"
+   puts "- help : displays this help message"
+   puts "- list : displays a list of songs you can play"
+   puts "- play : lets you choose a song to play"
+   puts "- exit : exits this program"
+end
+
+def play(playlist)    
+   puts "Please enter a song name or number:"
+   user_input = gets.chomp
+
+   song = playlist.select.with_index(1){|item, index|
+     user_input.to_i == index || user_input == item
+   }
+
+   if song.empty?
+   	 puts "Invalid input, please try again"
+   else
+        puts "Playing #{song.join}"
+   end
+end
+
+def list(playlist)
+	playlist.each.with_index(1){|song, index|
+      puts "#{index}. #{song}"
+	}
+end
+
+def exit_jukebox
+	puts "Goodbye"
+end
+
+def run(data)
+	response = ""
+    help
+	until response == "exit"
+	  puts "Please enter a command:"
+	  response = gets.chomp
+
+	 case response
+	 	when "help"
+	 		help
+	 	when "list"
+	 		list(data)
+	 	when "play"
+	 	    play(data)	
+	 	when "exit"
+	 		exit_jukebox
+	 	end
+	end	
+end
+
+
+
+
+
+
+
+
+
 
