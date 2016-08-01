6a6d0f48ca1d4378d0a85364488a57bc13ccc4d3
Max Etienne <etiemaxo@gmail.com>
2016-08-01 13:25:39 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +12,58 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+end
+
+def list(songs)
+  puts "1. Phoenix - 1901
+  2. Tokyo Police Club - Wait Up
+  3. Sufjan Stevens - Too Much
+  4. The Naked and the Famous - Young Blood
+  5. (Far From) Home - Tiga
+  6. The Cults - Abducted
+  7. Phoenix - Consolation Prizes
+  8. Harry Chapin - Cats in the Cradle
+  9. Amos Lee - Keep It Loose, Keep It Tight"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_response = gets.chomp
+  number = user_response.to_i
+  if songs.include?(user_response)
+    puts "Playing #{user_response}"
+  elsif (number > 0 && number <= songs.count)
+    puts "Playing #{songs[number - 1]}"
+  else
+    puts "Invalid input, please try again"
+  end
+
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  user_response = nil
+  while user_response != "exit"
+    puts "Please enter a command:"
+    user_response = gets.chomp
+    case user_response
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    end
+  end
+  exit_jukebox
+end
